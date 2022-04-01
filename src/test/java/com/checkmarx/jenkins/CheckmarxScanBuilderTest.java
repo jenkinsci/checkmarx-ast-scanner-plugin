8da797e8099377b38ad3ffd19e4ec5d2a99f86eb
Mithilesh Pawar <61288229+MithileshPawar@users.noreply.github.com>
2022-04-01 08:07:14 -0400

    Refactoring and Code Cleanup for Tests. (#36)
    * Updated the names of env variables for github actions
    
    * Updated Java wrapper to 1.0.33
    
    * Refactoring and code cleanup for tests.
    
    * Different EnvVars for AST Scan.
    
    * Reverting the change for workflows.
    
    * Changed the tag for team name in AST scan.

--- src/test/java/com/checkmarx/jenkins/CheckmarxScanBuilderTest.java
+++ src/test/java/com/checkmarx/jenkins/CheckmarxScanBuilderTest.java
@@ -1,6 +1,5 @@
 package com.checkmarx.jenkins;
 
-import com.checkmarx.jenkins.utils.Constants;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
@@ -11,40 +10,42 @@ import java.util.logging.Logger;
 
 public class CheckmarxScanBuilderTest extends CheckmarxTestBase {
 
-    private static final String CX_BASE_URI_ENV_VAR = String.format("${%s}",TEST_CX_BASE_URI);
-    private static final String CX_TENANT_ENV_VAR = String.format("${%s}",TEST_CX_TENANT);
-    private static final String CX_PROJECT_NAME_ENV_VAR = String.format("${%s}",TEST_CX_PROJECT_NAME);
-    private static final String CX_BRANCH_NAME_ENV_VAR = String.format("${%s}",TEST_CX_BRANCH_NAME);
+    private static final String CX_BASE_URI_ENV_VAR = String.format("${%s}", CheckmarxTestBase.CX_BASE_URI);
+    private static final String CX_TENANT_ENV_VAR = String.format("${%s}", CheckmarxTestBase.CX_TENANT);
+    private static final String CX_PROJECT_NAME_ENV_VAR = String.format("${%s}", CheckmarxTestBase.CX_PROJECT_NAME);
+    private static final String CX_BRANCH_NAME_ENV_VAR = String.format("${%s}", CheckmarxTestBase.CX_BRANCH);
 
     private static final Logger log = Logger.getLogger(CheckmarxScanBuilderTest.class.getName());
 
     @Test
     public void successCheckmarxScan() throws Exception {
         log.info("successCheckmarxScan");
-        runSuccessCheckmarxScan(this.astServerUrl, "successIntegrationJenkinsScan", this.astTenantName, "master");
+        this.runSuccessCheckmarxScan(this.astServerUrl, "JenkinsNormalScan", astTenantName, CheckmarxTestBase.BRANCH_MAIN);
     }
 
     @Test
     public void successCheckmarxScanWithEnvironmentVariables() throws Exception {
         log.info("successCheckmarxScanWithEnvironmentVariables");
-        runSuccessCheckmarxScan(CX_BASE_URI_ENV_VAR, CX_PROJECT_NAME_ENV_VAR, CX_TENANT_ENV_VAR, CX_BRANCH_NAME_ENV_VAR);
+        this.runSuccessCheckmarxScan(CX_BASE_URI_ENV_VAR, CX_PROJECT_NAME_ENV_VAR, CX_TENANT_ENV_VAR, CX_BRANCH_NAME_ENV_VAR);
     }
 
     @Test
     public void failWrongPresetCheckmarxScan() throws Exception {
         log.info("failWrongPresetCheckmarxScan");
 
-        final FreeStyleProject freeStyleProject = createSimpleProject("failWrongPresetCheckmarxScan");
+        String projectName = "JenkinsScanWithInvalidPreset";
+        final FreeStyleProject freeStyleProject = createSimpleProject(projectName);
 
         final CheckmarxScanBuilder checkmarxScanBuilder = new CheckmarxScanBuilder();
         checkmarxScanBuilder.setUseOwnServerCredentials(true);
-        checkmarxScanBuilder.setProjectName("failWrongPresetCheckmarxScan");
+        checkmarxScanBuilder.setProjectName(projectName);
         checkmarxScanBuilder.setServerUrl(this.astServerUrl);
         checkmarxScanBuilder.setUseAuthenticationUrl(StringUtils.isNotEmpty(this.astBaseAuthUrl));
         checkmarxScanBuilder.setBaseAuthUrl(this.astBaseAuthUrl);
         checkmarxScanBuilder.setTenantName(this.astTenantName);
-        checkmarxScanBuilder.setCheckmarxInstallation(Constants.JT_LATEST);
-        checkmarxScanBuilder.setCredentialsId(Constants.JT_TOKEN_ID);
+        checkmarxScanBuilder.setBranchName(CheckmarxTestBase.BRANCH_MAIN);
+        checkmarxScanBuilder.setCheckmarxInstallation(CheckmarxTestBase.JT_LATEST);
+        checkmarxScanBuilder.setCredentialsId(CheckmarxTestBase.JENKINS_CREDENTIALS_TOKEN_ID);
         checkmarxScanBuilder.setAdditionalOptions("--sast-preset-name Fake");
         checkmarxScanBuilder.setUseOwnAdditionalOptions(true);
 
@@ -59,17 +60,18 @@ public class CheckmarxScanBuilderTest extends CheckmarxTestBase {
     public void failWrongAdditionalOption() throws Exception {
         log.info("failWrongAdditionalOption");
 
-        final FreeStyleProject freeStyleProject = createSimpleProject("failWrongAdditionalOption");
+        String projectName = "JenkinsScanWithInvalidAdditionalOptions";
+        final FreeStyleProject freeStyleProject = createSimpleProject(projectName);
 
         final CheckmarxScanBuilder checkmarxScanBuilder = new CheckmarxScanBuilder();
         checkmarxScanBuilder.setUseOwnServerCredentials(true);
-        checkmarxScanBuilder.setProjectName("failWrongAdditionalOption");
+        checkmarxScanBuilder.setProjectName(projectName);
         checkmarxScanBuilder.setServerUrl(this.astServerUrl);
         checkmarxScanBuilder.setUseAuthenticationUrl(StringUtils.isNotEmpty(this.astBaseAuthUrl));
         checkmarxScanBuilder.setBaseAuthUrl(this.astBaseAuthUrl);
         checkmarxScanBuilder.setTenantName(this.astTenantName);
-        checkmarxScanBuilder.setCheckmarxInstallation(Constants.JT_LATEST);
-        checkmarxScanBuilder.setCredentialsId(Constants.JT_TOKEN_ID);
+        checkmarxScanBuilder.setCheckmarxInstallation(CheckmarxTestBase.JT_LATEST);
+        checkmarxScanBuilder.setCredentialsId(CheckmarxTestBase.JENKINS_CREDENTIALS_TOKEN_ID);
         checkmarxScanBuilder.setAdditionalOptions("--fakeproperty");
         checkmarxScanBuilder.setUseOwnAdditionalOptions(true);
 
@@ -83,15 +85,16 @@ public class CheckmarxScanBuilderTest extends CheckmarxTestBase {
     public void failWrongCheckmarxInstallation() throws Exception {
         log.info("failWrongCheckmarxInstallation");
 
-        final FreeStyleProject freeStyleProject = createSimpleProject("failWrongCheckmarxInstallation");
+        String projectName = "JenkinsScanWithInvalidCheckmarxInstallation";
+        final FreeStyleProject freeStyleProject = createSimpleProject(projectName);
 
         final CheckmarxScanBuilder checkmarxScanBuilder = new CheckmarxScanBuilder();
-        checkmarxScanBuilder.setProjectName("failWrongCheckmarxInstallation");
+        checkmarxScanBuilder.setProjectName(projectName);
         checkmarxScanBuilder.setServerUrl(this.astServerUrl);
         checkmarxScanBuilder.setUseAuthenticationUrl(StringUtils.isNotEmpty(this.astBaseAuthUrl));
         checkmarxScanBuilder.setBaseAuthUrl(this.astBaseAuthUrl);
         checkmarxScanBuilder.setTenantName(this.astTenantName);
-        checkmarxScanBuilder.setCredentialsId(Constants.JT_TOKEN_ID);
+        checkmarxScanBuilder.setCredentialsId(CheckmarxTestBase.JENKINS_CREDENTIALS_TOKEN_ID);
         checkmarxScanBuilder.setUseOwnServerCredentials(true);
 
         freeStyleProject.getBuildersList().add(checkmarxScanBuilder);
@@ -105,15 +108,15 @@ public class CheckmarxScanBuilderTest extends CheckmarxTestBase {
     public void failIfProjectNameNotProvided() throws Exception {
         log.info("failIfProjectNameNotProvided");
 
-        final FreeStyleProject freeStyleProject = createSimpleProject("failIfProjectNameNotProvided");
+        final FreeStyleProject freeStyleProject = createSimpleProject("ScanWithoutProjectName");
 
         final CheckmarxScanBuilder checkmarxScanBuilder = new CheckmarxScanBuilder();
         checkmarxScanBuilder.setServerUrl(this.astServerUrl);
         checkmarxScanBuilder.setUseAuthenticationUrl(StringUtils.isNotEmpty(this.astBaseAuthUrl));
         checkmarxScanBuilder.setBaseAuthUrl(this.astBaseAuthUrl);
         checkmarxScanBuilder.setTenantName(this.astTenantName);
-        checkmarxScanBuilder.setCheckmarxInstallation(Constants.JT_LATEST);
-        checkmarxScanBuilder.setCredentialsId(Constants.JT_TOKEN_ID);
+        checkmarxScanBuilder.setCheckmarxInstallation(CheckmarxTestBase.JT_LATEST);
+        checkmarxScanBuilder.setCredentialsId(CheckmarxTestBase.JENKINS_CREDENTIALS_TOKEN_ID);
         checkmarxScanBuilder.setUseOwnServerCredentials(true);
 
         freeStyleProject.getBuildersList().add(checkmarxScanBuilder);
@@ -126,14 +129,14 @@ public class CheckmarxScanBuilderTest extends CheckmarxTestBase {
     /**
      * Runs a checkmarx scan successfully
      *
-     * @param serverUrl - ast server url
+     * @param serverUrl   - ast server url
      * @param projectName - ast project name
-     * @param tenant - ast tenant
-     * @param branch - ast branch
+     * @param tenant      - ast tenant
+     * @param branch      - ast branch
      * @throws Exception
      */
     private void runSuccessCheckmarxScan(String serverUrl, String projectName, String tenant, String branch) throws Exception {
-        final FreeStyleProject freeStyleProject = createSimpleProject("successIntegrationJenkinsScan");
+        final FreeStyleProject freeStyleProject = createSimpleProject("JenkinsNormalScan");
 
         final CheckmarxScanBuilder checkmarxScanBuilder = new CheckmarxScanBuilder();
         checkmarxScanBuilder.setUseOwnServerCredentials(true);
@@ -143,8 +146,8 @@ public class CheckmarxScanBuilderTest extends CheckmarxTestBase {
         checkmarxScanBuilder.setBranchName(branch);
         checkmarxScanBuilder.setUseAuthenticationUrl(StringUtils.isNotEmpty(this.astBaseAuthUrl));
         checkmarxScanBuilder.setBaseAuthUrl(this.astBaseAuthUrl);
-        checkmarxScanBuilder.setCheckmarxInstallation(Constants.JT_LATEST);
-        checkmarxScanBuilder.setCredentialsId(Constants.JT_TOKEN_ID);
+        checkmarxScanBuilder.setCheckmarxInstallation(CheckmarxTestBase.JT_LATEST);
+        checkmarxScanBuilder.setCredentialsId(CheckmarxTestBase.JENKINS_CREDENTIALS_TOKEN_ID);
         checkmarxScanBuilder.setAdditionalOptions("--scan-types sast");
         checkmarxScanBuilder.setUseOwnAdditionalOptions(true);
 
