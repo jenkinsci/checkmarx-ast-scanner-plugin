cc227064a0920eac02a21af3ac03022beb26624e
tamarleviCm <tamar.levi@checkmarx.com>
2024-04-10 07:54:52 +0200

    AST-38150 order code

--- src/main/java/com/checkmarx/jenkins/tools/ProxyHttpClient.java
+++ src/main/java/com/checkmarx/jenkins/tools/ProxyHttpClient.java
@@ -1,7 +1,6 @@
 package com.checkmarx.jenkins.tools;
 
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URI;
@@ -9,52 +8,40 @@ import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.*;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.apache.commons.lang.StringUtils;
 
 
 public class ProxyHttpClient {
 
     public OkHttpClient getHttpClient(String proxyString, int connectionTimeoutMillis, int readTimeoutMillis) throws URISyntaxException {
+        OkHttpClient.Builder okClientBuilder = new OkHttpClient.Builder()
+                .connectTimeout(connectionTimeoutMillis, TimeUnit.MILLISECONDS)
+                .readTimeout(readTimeoutMillis, TimeUnit.MILLISECONDS);
         if (proxyString != null) {
-            URI proxyUrl = new URI(proxyString);
-            String proxyHost = proxyUrl.getHost();
-            int proxyPort = proxyUrl.getPort();
-            String proxyUserPass = proxyUrl.getUserInfo();
-            if ((!proxyHost.isEmpty()) && (proxyPort >= 10) && (proxyPort <= 65535)) {
-                Proxy _httpProxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
-                if (StringUtils.isNotEmpty(proxyUserPass)) {
-                    String[] userPass = proxyUserPass.split(":");
-                    Authenticator _httpProxyAuth = new Authenticator() {
-                        @Override
-                        public Request authenticate(@NotNull Route route, @NotNull Response response)
-                                throws IOException {
-                            String credential = Credentials.basic(userPass[0], userPass[1]);
-                            return response
-                                    .request()
-                                    .newBuilder()
-                                    .header("Proxy-Authorization", credential)
-                                    .build();
-                        }
-                    };
-                    return new OkHttpClient.Builder()
-                            .proxy(_httpProxy)
-                            .proxyAuthenticator(_httpProxyAuth)
-                            .connectTimeout(connectionTimeoutMillis, TimeUnit.MILLISECONDS)
-                            .readTimeout(readTimeoutMillis, TimeUnit.MILLISECONDS)
-                            .build();
+            URI proxy = new URI(proxyString);
+            if (isValidProxy(proxy.getHost(), proxy.getPort())) {
+                Proxy _httpProxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxy.getHost(), proxy.getPort()));
+                if (StringUtils.isNotEmpty(proxy.getUserInfo())) {
+                    String[] userPass = proxy.getUserInfo().split(":");
+                    Authenticator _httpProxyAuth = getAuthenticator(userPass);
+                    return okClientBuilder.proxyAuthenticator(_httpProxyAuth).proxy(_httpProxy).build();
                 } else {
-                    return new OkHttpClient.Builder()
-                            .proxy(_httpProxy)
-                            .connectTimeout(connectionTimeoutMillis, TimeUnit.MILLISECONDS)
-                            .readTimeout(readTimeoutMillis, TimeUnit.MILLISECONDS)
-                            .build();
+                    return okClientBuilder.proxy(_httpProxy).build();
                 }
             }
         }
-        return new OkHttpClient.Builder()
-                .connectTimeout(connectionTimeoutMillis, TimeUnit.MILLISECONDS)
-                .readTimeout(readTimeoutMillis, TimeUnit.MILLISECONDS)
-                .build();
+        return okClientBuilder.build();
+    }
+
+    private static Authenticator getAuthenticator(String[] userPass) {
+        return (route, response) -> {
+            String credential = Credentials.basic(userPass[0], userPass[1]);
+            return response.request().newBuilder()
+                    .header("Proxy-Authorization", credential).build();
+        };
+    }
+
+    private static boolean isValidProxy(String proxyHost, int proxyPort) {
+        return (!proxyHost.isEmpty()) && (proxyPort >= 10) && (proxyPort <= 65535);
     }
 }
