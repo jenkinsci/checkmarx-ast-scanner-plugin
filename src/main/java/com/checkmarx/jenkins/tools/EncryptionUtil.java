ecd6d9055bf2909e11640a78e1f0491a8ed2c7f7
miryamfoiferCX <miryam.foifer@checkmarx.com>
2024-09-05 12:22:34 +0300

    fix spotBug

--- src/main/java/com/checkmarx/jenkins/tools/EncryptionUtil.java
+++ src/main/java/com/checkmarx/jenkins/tools/EncryptionUtil.java
@@ -3,6 +3,7 @@ package com.checkmarx.jenkins.tools;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class EncryptionUtil {
@@ -11,7 +12,7 @@ public class EncryptionUtil {
     public static String encrypt(String data, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        byte[] encryptedData = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encryptedData);
     }
 
