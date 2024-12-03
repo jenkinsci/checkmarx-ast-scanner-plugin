33acc5eedbe9383d4c062f08610bd2f3cbdc545a
galactica <tiago.baptista@checkmarx.com>
2024-12-03 16:51:17 +0200

    support in space

--- src/main/java/com/checkmarx/jenkins/CheckmarxScanBuilder.java
+++ src/main/java/com/checkmarx/jenkins/CheckmarxScanBuilder.java
@@ -382,9 +382,8 @@ public class CheckmarxScanBuilder extends Builder implements SimpleBuildStep {
                     String fileName = (additionalOptions.contains("--output-name")
                             ? extractOptionValue(additionalOptions, "--output-name")
                             : PluginUtils.defaultOutputName) + "." + formatType;
-                    String outputPath = additionalOptions.contains("--output-path")
-                            ? extractOptionValue(additionalOptions, "--output-path")
-                            : workspace.getRemote();
+                    String outputPath =  extractOptionValue(additionalOptions, "--output-path");
+
                     String fullFilePath = new File(outputPath, fileName).getPath();
                     FilePath destinationPath = workspace.child(fileName);
 
@@ -408,7 +407,15 @@ public class CheckmarxScanBuilder extends Builder implements SimpleBuildStep {
         if (options.contains(optionKey)) {
             String[] parts = options.split(optionKey, 2);
             if (parts.length > 1) {
-                return parts[1].trim().split(" ")[0];
+                String remaining = parts[1].trim();
+                if (remaining.startsWith("\"")) {
+                    int endIndex = remaining.indexOf("\"", 1);
+                    if (endIndex > 0) {
+                        return remaining.substring(1, endIndex);
+                    }
+                } else {
+                    return remaining.split(" ")[0];
+                }
             }
         }
         return "";
