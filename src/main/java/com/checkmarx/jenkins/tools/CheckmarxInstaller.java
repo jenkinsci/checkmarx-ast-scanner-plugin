7afe4d1a0430e1859913de73e8f5b9404e7a55a3
AlvoBen <alvo@post.bgu.ac.il>
2025-02-27 13:29:52 +0200

    Change defaultVersion to static in CheckmarxInstaller

--- src/main/java/com/checkmarx/jenkins/tools/CheckmarxInstaller.java
+++ src/main/java/com/checkmarx/jenkins/tools/CheckmarxInstaller.java
@@ -54,7 +54,7 @@ public class CheckmarxInstaller extends ToolInstaller {
 
     private static final String INSTALLED_FROM = ".installedFrom";
     private static final String TIMESTAMP_FILE = ".timestamp";
-    private final String defaultVersion = "2.3.14";
+    private static final String defaultVersion = "2.3.14";
     private final String version;
     private final Long updatePolicyIntervalHours;
     private CxLoggerAdapter log;
